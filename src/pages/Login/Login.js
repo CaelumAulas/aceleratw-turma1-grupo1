2f9f23fa8824a39f715aee3b1690384eed5f7389
Miguel Soares <oieusouamiguel@gmail.com>
2021-05-05 16:25:21 -0300

    feat(login-form): add container

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 import BaseLayout from '../../containers/BaseLayout/BaseLayout'
+import LoginForm from '../../containers/LoginForm/LoginForm'
 
 const Login = () => (
     <BaseLayout pageName="Login">
-        Login
+        <LoginForm />
     </BaseLayout>
 )
 
