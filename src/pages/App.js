1436da77f2874c0cd0b80992a5be071f7d40014f
stellatw <stella.martins@thoughtworks.com>
2021-05-11 15:27:47 -0300

    feat(users-form): merge with develop

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,34 +8,34 @@ import SignUp from "../pages/SignUp/SignUp";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Users from "../pages/Users/Users";
 import Brands from "../pages/Brands/Brands";
-import RegisterVehicles from './RegisterVehicles/RegisterVehicles';
+import AddUser from "../pages/AddUser/AddUser";
 
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/usuarios">
-            <Users />
-          </Route>
-          <Route path="/marcas">
-            <Brands />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/cadastro-veiculo">
-            <RegisterVehicles />
-          </Route>
-          <Route path="/">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/usuarios/cadastrar-usuarios">
+              <AddUser />
+            </Route>
+            <Route path="/usuarios">
+              <Users />
+            </Route>
+            <Route path="/marcas">
+              <Brands />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
   );
 };
 
